777d94b64bc6e0928f515c44aa989307b2fb94d9
Solmaz <Solmazgharagozloo@gmail.com>
2022-03-06 11:52:45 -0800

    fixed chrome driver path

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 @pytest.fixture(scope="class")
 def setup(request):
     # service = Service("/Users/solmaz.gharagozloo/Desktop/chromedriver")
-    path = "/Users/solmaz.gharagozloo/Desktop/chromedriver"
+    path = "C:\\Users\\Solmaz\\Desktop\\chromedriver.exe"
     driver = webdriver.Chrome(executable_path=path)
     driver.get('https://www.therabody.com/')
     driver.maximize_window()
